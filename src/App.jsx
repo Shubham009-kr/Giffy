d4143bfb0ed11c935f64736c7e17548601fdd283
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-21 20:29:15 +0530

    added api routes, redux store and search slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const App = () => {
+  
   return (
-    <div className='w-full h-screen bg-[#222222] text-[#f7ebff]'>App</div>
+    <div className='w-full h-screen bg-[#222222] text-[#f7ebff]'>
+      <button>get</button>
+    </div>
   )
 }
 
