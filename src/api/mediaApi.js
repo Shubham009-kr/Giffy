aa0eb5bbe9429920bb4da92b87bb727b7e4a928b
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-22 23:50:23 +0530

    updated environmet variables and grid section

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY
 
@@ -11,7 +11,7 @@ export async function fetchPhotos(query, page=1, per_page=20){
         headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`},
     })
     
-    return response.data;
+    return response.data.results;
 }
 
 export async function fetchVideos(query, per_page=15){
@@ -20,13 +20,13 @@ export async function fetchVideos(query, per_page=15){
         headers:{Authorization:PEXELS_KEY},
     })
     
-    return response.data;
+    return response.data.videos;
 }
 
 export async function fetchGif(query, limit=20){
     const response = await axios.get('https://api.giphy.com/v1/gifs/search',{
-        params:{q: query, key: GIPHY_KEY, limit },
+        params:{q: query, api_key: GIPHY_KEY, limit },
     })
     
-    return response;
+    return response.data.data;
 }
--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -5,10 +5,39 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const ResultGrid = () => {
     const dispatch = useDispatch();
-    const {query, activeTabs, results, loading, error} = useSelector((store)=>store.search)
-    useEffect()
+    const {query, activeTab, results, loading, error} = useSelector((store)=>store.search)
+    const getData = async ()=>{
+      let data;
+      let response;
+      if(activeTab == 'photos'){
+        response = await fetchPhotos(query);
+        data = response.map((item)=>({
+          id:item.id,
+          type:'photo',
+          title:item.alt_description,
+          thumbnail:item.urls.small,
+          src:item.urls.full,
+        }))  
+      }
+      if(activeTab == 'videos'){
+        response = await fetchVideos(query);
+        
+      }
+      if(activeTab == 'GIF'){
+        response = await fetchGif(query);
+
+      }
+      console.log(data);
+
+    }
+
+    useEffect(()=>{
+      getData();
+    },[query, activeTab]);
+
   return (
     <div>
+      <button>Get Data</button>
 
     </div>
   )
